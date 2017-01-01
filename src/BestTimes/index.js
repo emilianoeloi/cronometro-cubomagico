46a72847ba722843a163936510803a78fb0f6437
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-01-01 00:18:55 -0200

    CHANGE #22: colocar hancking stático na página não logada

--- src/BestTimes/index.js
+++ src/BestTimes/index.js
@@ -8,7 +8,12 @@ class BestTimes extends Component {
   render() {
     let i = 1;
     if (!this.props.times || this.props.times.length === 0) {
-      return null;
+      return (
+        <table className="pure-table">
+          <caption className="Table-caption">
+            <h2>Melhores Tempos</h2>
+          </caption><thead><tr><th>#</th><th>Data</th><th>Cubista</th><th>Tempo</th></tr></thead><tbody><tr class=""><td>1</td><td>12/15/2016</td><td>Darlene Medeiros</td><td class="time-formated">01' 05.00"</td></tr><tr class="pure-table-odd"><td>2</td><td>12/16/2016</td><td>Aline Villaça</td><td class="time-formated">01' 06.00"</td></tr><tr class=""><td>3</td><td>12/28/2016</td><td>Emiliano Eloi S. Barbosa (Eloi)</td><td class="time-formated">01' 06.00"</td></tr><tr class="pure-table-odd"><td>4</td><td>12/16/2016</td><td>Igor Canêdo</td><td class="time-formated">01' 09.00"</td></tr><tr class=""><td>5</td><td>12/30/2016</td><td>PAULO ROBERTO ROSA NASCIMENTO</td><td class="time-formated">01' 46.63"</td></tr><tr class="pure-table-odd"><td>6</td><td>12/31/2016</td><td>Rhyana Patrícia</td><td class="time-formated">02' 44.38"</td></tr><tr class=""><td>7</td><td>12/30/2016</td><td>Jujuda 2007</td><td class="time-formated">03' 38.40"</td></tr></tbody></table>
+      );
     }
     return (
       <table className="pure-table">
